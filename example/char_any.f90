1084f19cccbe08ea5941ef08ce14287899883246
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-04-26 01:02:16 +0900

    updated an example

--- example/char_any.f90
+++ example/char_any.f90
@@ -16,4 +16,7 @@ program char_any
 
     print *, as_int32(map%get("int32.max")) ! 2147483647
     print *, as_real64(map%get("real64.max")) ! 1.7976931348623157E+308
+
+    call map%remove_if("real64.max", -huge(0._real64))
+    print *, map%contains("real64.max") ! T
 end program char_any
