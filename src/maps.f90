bcc6920ea9687901ea3819535f23564cbb15a609
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-04-26 00:30:37 +0900

    added udt to maps module

--- src/maps.f90
+++ src/maps.f90
@@ -1,3 +1,17 @@
 module maps
     use :: map_char_int32
+    use :: map_char_any
+    use :: map_type_mappable, only: &
+        as_int8, &
+        as_int16, &
+        as_int32, &
+        as_int64, &
+        as_real32, &
+        as_real64, &
+        as_real128, &
+        as_complex32, &
+        as_complex64, &
+        as_complex128, &
+        as_logical, &
+        as_char
 end module maps
