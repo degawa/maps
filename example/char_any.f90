7278facf1c817cf33329db904579f06586337a1c
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-04-26 01:09:39 +0900

    added example of char_to_any_map_type

--- example/char_any.f90
+++ example/char_any.f90
@@ -17,6 +17,15 @@ program char_any
     print *, as_int32(map%get("int32.max")) ! 2147483647
     print *, as_real64(map%get("real64.max")) ! 1.7976931348623157E+308
 
+    block
+        integer(int8) :: i8
+        real(real32) :: r32
+        i8 = map%get("int8.max")
+        r32 = map%get("real32.max")
+
+        print *, i8, r32 ! 127   3.40282347E+38
+    end block
+
     call map%remove_if("real64.max", -huge(0._real64))
     print *, map%contains("real64.max") ! T
 end program char_any
