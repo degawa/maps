9b3068f657838bb271f1a5af1cbeff378269b37e
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-04-27 23:33:01 +0900

    added an example

---
+++ example/char_int32_err.f90
@@ -0,0 +1,21 @@
+program char_int32_err
+    use, intrinsic :: iso_fortran_env
+    use :: maps
+    use :: errstat
+    implicit none
+
+    type(char_to_int32_map_type) :: map
+    type(error_stat_type) :: status
+
+    call map%initialize(status=status)
+
+    call map%put("apple", 100, status)
+    call map%put("apple", 200, status)
+    if (error_occurred(status)) then
+        print *, status%get_message()
+    end if
+
+    print *, map%get("apple") ! 100
+
+    call map%finalize(status)
+end program char_int32_err
