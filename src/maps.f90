e978824f3c7b67681c69032b4e9d228b39536d3f
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-08-10 00:51:27 +0900

    updated maps module

--- src/maps.f90
+++ src/maps.f90
@@ -1,7 +1,8 @@
 module maps
-    use :: map_char_int32
-    use :: map_char_any
-    use :: map_any_int32
+    use :: map_char_int32, only:char_to_int32_map_type
+    use :: map_char_any, only:char_to_any_map_type
+    use :: map_any_int32, only:any_to_int32_map_type
+    use :: map_bitset_int32, only:bitset_to_int32_map_type
     use :: maps_common_type_mappable, only: &
         as_int8, &
         as_int16, &
@@ -17,4 +18,26 @@ module maps
         as_char
     use :: maps_common_type_hashFunction, only:hash_functions
     use :: maps_common_type_collisionResolver, only:collision_resolvers
+    implicit none
+    ! user-defined types
+    public :: char_to_int32_map_type
+    public :: char_to_any_map_type
+    public :: any_to_int32_map_type
+    public :: bitset_to_int32_map_type
+    ! procedures
+    public :: as_int8, &
+              as_int16, &
+              as_int32, &
+              as_int64, &
+              as_real32, &
+              as_real64, &
+              as_real128, &
+              as_complex32, &
+              as_complex64, &
+              as_complex128, &
+              as_logical, &
+              as_char
+    ! typed enumerator
+    public :: hash_functions
+    public :: collision_resolvers
 end module maps
