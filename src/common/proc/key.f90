a9e43c79f8f17dd0a2cd93706e9a7a2ff993a902
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-11-14 10:56:49 +0900

    added proc to get the byte of key%value

--- src/common/proc/key.f90
+++ src/common/proc/key.f90
@@ -4,6 +4,7 @@ module maps_common_proc_key
     implicit none
     private
     public :: to_settable
+    public :: get_byte_of_component
     public :: to_char
 
 contains
@@ -21,6 +22,17 @@ contains
 
         key_int8 = transfer(key, mold=0_int8, size=num_int8_elem)
     end function to_settable
+
+    !>Returns the byte of key's component `value`.
+    pure elemental function get_byte_of_component(key) result(value_byte)
+        implicit none
+        type(key_type), intent(in) :: key
+            !! a key
+        integer(int32) :: value_byte
+            !! byte of `key%value`
+
+        value_byte = size(key%value)
+    end function get_byte_of_component
 
     !>Returns character converted from a key.
     function to_char(key) result(key_as_char)
